a77e5f2fc0665606586849dc42b27619b7843eaf
PKM-UK <pyrophernalia@gmail.com>
2024-11-15 14:49:19 +0000

    Greedy and lookahead algorithms, run ten quiet games and look for different outcomes

--- NineCoins.py
+++ NineCoins.py
@@ -25,20 +25,71 @@ class NineCoinsBoard(Board):
             takenCoin = self.board.pop()
         self.scores[self.nextPlayer] = self.scores[self.nextPlayer] + takenCoin
 
-        print(f'Player {self.nextPlayer} takes {takenCoin} to make {self.scores[self.nextPlayer]}')
-        print(f'Board is now {self.board}')
+        # print(f'Player {self.nextPlayer} takes {takenCoin} to make {self.scores[self.nextPlayer]}')
+        # print(f'Board is now {self.board}')
         if len(self.board) < 1:
             self.terminated = True
         self.nextPlayer = 1 - self.nextPlayer
 
 class NineCoinsPlayer(Player):
+    def __init__(self, algorithm):
+        self.algorithm = algorithm
+        
+    def greedyValue(self, board, direction):
+        # Value functions return the expected value of a direction to the player taking the turn
+        if direction == 'L':
+            return board[0]
+        else:
+            return board[-1]
+
+    def greedyPick(self, board):
+        # Pick functions return (action, value)
+        if len(board) == 1:
+            return ('L', board[0])
+        else:
+            lValue = self.greedyValue(board, 'L')
+            rValue = self.greedyValue(board, 'R')
+            # print(f'L gives {lValue}, R gives {rValue}')
+            return ('L', lValue) if lValue > rValue else ('R', rValue)
+        
+    def oneLookaheadValue(self, board, direction):
+        if len(board) == 1:
+            # We get the one coinn
+            return board[0]
+        elif len(board) == 2:
+            # We pick one, opponent picks the other
+            if direction == 'L':
+                return board[0] - board[1]
+            else:
+                return board[1] - board[0]
+        else:
+            # There are three or coins - return ours minus greedy pick of the remainder
+            if direction == 'L':
+                # This could be board[0] - greedyValue(board[1:])[0]
+                # That's how to write recursive minmax
+                # print(f'L would return {board[0] - max(board[1], board[-1])} ({board[0]} minus {max(board[1], board[-1])})')
+                return board[0] - max(board[1], board[-1])
+            else:
+                # print(f'R would return {board[-1] - max(board[0], board[-2])} ({board[-1]} minus {max(board[0], board[-2])})')
+                return board[-1] - max(board[0], board[-2])
+            
+    def oneLookAheadPick(self, board):
+        # Pick functions return (action, value)
+        if len(board) == 1:
+            return ('L', board[0])
+        else:
+            lValue = self.oneLookaheadValue(board, 'L')
+            rValue = self.oneLookaheadValue(board, 'R')
+            return ('L', lValue) if lValue > rValue else ('R', rValue)        
+             
     def takeTurn(self, state):
         # Algorithm here! Factor out later
         action = ''
-        if state.board[0] > state.board[-1]:
-            action = 'L'
+        if self.algorithm == 0:
+            action = self.greedyPick(state.board)[0]
         else:
-            action = 'R'
+            action = self.oneLookAheadPick(state.board)[0]
+
         turn = NineCoinsTurn(action)
         return turn
 
--- Tournament.py
+++ Tournament.py
@@ -3,16 +3,35 @@ from NineCoins import *
 
 class Tournament:
     def runNineCoins(self):
-        players = [NineCoinsPlayer(), NineCoinsPlayer()]
+        
+        # For i in 1 to 10:
+        # play the players one way, set the board seed to i, play the players the other way, set the board seed again
+        for seed in range(1, 11):
+          print(f'Playing game {seed}')
+          players = [NineCoinsPlayer(0), NineCoinsPlayer(1)]
+          board = NineCoinsBoard()
+          game = Game(players, board)
+          game.setup(seed)
+          endState = game.loop()
 
-        board = NineCoinsBoard()
+          gameOneScores = endState.scores
 
-        game = Game(players, board)
+          print(endState)
 
-        game.setup(0)
-        endState = game.loop()
+          players = [NineCoinsPlayer(1), NineCoinsPlayer(0)]
+          game = Game(players, board)
+          game.setup(seed)
+          endState = game.loop()
 
-        print(endState)
+          gameTwoScores = endState.scores
+
+          print(endState)
+
+          if gameOneScores[0] != gameTwoScores[0]:
+              print ("Different scores!")
+          if (gameOneScores[0] > gameOneScores[1] and gameTwoScores[1] > gameTwoScores[0]) or (gameOneScores[0] < gameOneScores[1] and gameTwoScores[1] < gameTwoScores[0]):
+              print ("DIFFERENT WINNER!")
+
 
 tourney = Tournament()
 tourney.runNineCoins()
